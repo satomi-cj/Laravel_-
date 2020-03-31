9353a06d617d6bfbf8ec03174877db055e9ea06c
suzuki satomi <coronjill@gmail.com>
2020-03-31 05:07:45 +0000

    Bladeテンプレートの継承とLaravelMix_課題

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv-"X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, intial-scale=1">
-        
-        <title>profile</title>
-    </head>
-    <body>
-        <hi>profile</hi>
-    </body>
-</html>
+@extends('layouts.profile')
+
+@section('title', 'プロフィール')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>MY プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
