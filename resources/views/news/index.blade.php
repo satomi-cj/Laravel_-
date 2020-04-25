d308b9e0b21993eb82068b69d064866c37b627da
suzuki satomi <coronjill@gmail.com>
2020-04-25 11:15:22 +0000

    再コミット

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -5,6 +5,7 @@
 @section('content')
     <div class="container">
         <hr color="#c0c0c0">
+        <p>ニューストップページ</p>
         @if (!is_null($headline))
             <div class="row">
                 <div class="headline col-md-10 mx-auto">
